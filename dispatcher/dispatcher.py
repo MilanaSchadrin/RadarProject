638d46e03c9e6ecd90840c19c77efbba157c1f6e
Ivan S <issibirtsev@gmail.com>
2025-03-28 01:21:15 +0300

    short dispatcher

--- dispatcher/dispatcher.py
+++ dispatcher/dispatcher.py
@@ -1,13 +1,18 @@
+from queue import PriorityQueue
+
+
 class Dispatcher:
     def __init__(self):
         self.currentTime = 0
         self.messageQueues = {}
 
-    def register(self):
-        pass
+    def register(self, recipient_id, messages_type=0):
+        self.messageQueues[(recipient_id, messages_type)] = PriorityQueue()
 
-    def send_message(self):
-        pass
+    def send_message(self, message, recipient_id, messages_type=0):
+        if not (recipient_id, messages_type) in self.messageQueues:
+            self.register(recipient_id, messages_type)
+        self.messageQueues[(recipient_id, messages_type)].put((message.priority.value, message))
 
-    def get_message(self):
-        pass
+    def get_message(self, recipient_id, messages_type=0):
+        return self.messageQueues.get((recipient_id, messages_type), PriorityQueue())
--- dispatcher/messages.py
+++ dispatcher/messages.py
@@ -1,11 +1,32 @@
 from dataclasses import dataclass
+from typing import Union, Dict, List
+from enums import Priorities, Modules
 
 
 @dataclass
 class Message:
-    recipient_id: int
-    priority: int
+    recipient_id: Union[Modules.GUI, Modules.RadarMain, Modules.SE,
+                        Modules.LauncherMain, Modules.ControlCenter]
+    priority: Union[Priorities.LOW, Priorities.STANDARD, Priorities.HIGH]
     timeSend: int
 
-    def get_data(self):
-        pass
+
+@dataclass
+class SEUpdateRocket(Message):
+    rocket: Dict[int, List[float]]
+
+
+@dataclass
+class SEStarting(Message):
+    planes: Dict[int, List[float]]
+
+
+@dataclass
+class PlaneKilled(Message):
+    killed: Dict[str, Dict[int, List[float]]]
+    collateralDamage: List[int]
+
+
+@dataclass
+class AddRocket(Message):
+    rocket: Dict[int, List[float]]
