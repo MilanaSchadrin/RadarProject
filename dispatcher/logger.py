d82fbe8fac49daea82aefea07bb72a3907c6a934
Ivan S <issibirtsev@gmail.com>
2025-04-17 23:28:46 +0300

    Logger class update

--- dispatcher/logger.py
+++ dispatcher/logger.py
@@ -1,36 +1,57 @@
 import json
+from pathlib import Path
+from datetime import datetime
 
 
-def convert_missile_to_json(missile):
-    js = vars(missile)
-    js['status'] = js['status'].name
-    js['missileType'] = js['missileType'].name
-    return js
+class Logger:
+    def __init__(self, log_path: Path):
+        self.path = log_path
 
+    @staticmethod
+    def convert_missile_to_json(missile):
+        js = vars(missile)
+        js['status'] = js['status'].name
+        js['missileType'] = js['missileType'].name
+        return js
 
-def convert_target_to_json(target):
-    js = vars(target)
-    js['status'] = js['status'].name
-    for key in js['attachedMissiles'].keys():
-        js['attachedMissiles'][key] = convert_missile_to_json(js['attachedMissiles'][key])
-    return js
+    @staticmethod
+    def convert_target_to_json(target):
+        js = vars(target)
+        js['status'] = js['status'].name
+        for key in js['attachedMissiles'].keys():
+            js['attachedMissiles'][key] = Logger.convert_missile_to_json(js['attachedMissiles'][key])
+        return js
 
+    @staticmethod
+    def convert_message_to_json(message):
+        js = vars(message)
+        for key in js.keys():
+            if key == 'priority':
+                js[key] = js[key].name
+            elif key == 'recipient_id':
+                js[key] = js[key].name
+            elif key == 'missile':
+                js[key] = Logger.convert_missile_to_json(js[key])
+            elif key == 'missiles':
+                js[key] = list(map(Logger.convert_missile_to_json, js[key]))
+            elif key == 'target':
+                js[key] = Logger.convert_target_to_json(js[key])
+            elif key == 'detected_objects':
+                js[key] = list(map(Logger.convert_target_to_json, js[key]))
+        js = {type(message).__name__: js}
+        return js
 
-def convert_message_to_json(message):
-    js = vars(message)
-    for key in js.keys():
-        if key == 'priority':
-            js[key] = js[key].name
-        elif key == 'recipient_id':
-            js[key] = js[key].name
-        elif key == 'missile':
-            js[key] = convert_missile_to_json(js[key])
-        elif key == 'missiles':
-            js[key] = list(map(convert_missile_to_json, js[key]))
-        elif key == 'target':
-            js[key] = convert_target_to_json(js[key])
-        elif key == 'detected_objects':
-            js[key] = list(map(convert_target_to_json, js[key]))
-    js = {type(message).__name__: js}
-    return js
+    def log(self, message, current_step):
 
+        json_dir = self.path / "jsons"
+        json_dir.mkdir(parents=True, exist_ok=True)
+
+        timestamp = datetime.now().strftime("%d_%m_%Y-%H:%M:%S")
+        file_path = json_dir / f"{current_step}.json"
+        data = {}
+        if file_path.exists():
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+        data[timestamp] = self.convert_message_to_json(message)
+        with open(file_path, "w") as f:
+            json.dump(data, f)
