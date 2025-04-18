d3bf2ea68353f90d433f8c130575f66f5a29fc9a
Milana Schadrin <107313847+MilanaSchadrin@users.noreply.github.com>
2025-04-18 13:28:18 +0300

    Revert "Adding loger"

--- dispatcher/messages.py
+++ dispatcher/messages.py
@@ -6,17 +6,14 @@ from radar.Target import Target
 import numpy as np
 from numpy.typing import NDArray
 
-
-@dataclass(order=True)
+@dataclass(order=True) 
 class Message:
     recipient_id: Union[Modules, int]
     priority: Union[Priorities, int]
 
-
 @dataclass
 class SEStarting(Message):
     planes: Dict[int, NDArray[np.float64]]
-
 
 @dataclass
 class SEKilled(Message):
@@ -26,20 +23,15 @@ class SEKilled(Message):
     plane_id: int
     plane_coords: NDArray[np.float64]
     collateral_damage: List[Tuple[int, NDArray[np.float64]]]
-
-
 @dataclass
 class RocketInactivated(Message):
-    rocket_id: int
-    timeStep: int
-
-
+    rocket_id:int
+    timeStep:int
 @dataclass
 class SEAddRocket(Message):
     startTime: int
     rocket_id: int
     rocket_coords: NDArray[np.float64]
-
 
 @dataclass
 class SEAddRocketToRadar(Message):
@@ -48,22 +40,19 @@ class SEAddRocketToRadar(Message):
     missile: Missile
     rocket_coords: NDArray[np.float64]
 
-
 @dataclass
 class CCLaunchMissile(Message):
     target: Target
 
 
-@dataclass(order=True)
+@dataclass(order=True) 
 class CCToRadarNewStatus(Message):
     priority_s: int
     new_target_status: Tuple[int, int]
 
-
 @dataclass
 class CCToSkyEnv(Message):
     missiles: List[Missile]
-
 
 @dataclass
 class RadarToGUICurrentTarget(Message):
@@ -71,22 +60,18 @@ class RadarToGUICurrentTarget(Message):
     target_id: str
     sector_size: float
 
-
 @dataclass
 class RadarControllerObjects(Message):
     detected_objects: List[Target]
-
 
 @dataclass
 class LaunchertoSEMissileLaunched(Message):
     targetId: int
     missile: Missile
 
-
 @dataclass
 class LaunchertoCCMissileLaunched(Message):
     missile: Missile
-
 
 @dataclass
 class ToGuiRocketInactivated(Message):
